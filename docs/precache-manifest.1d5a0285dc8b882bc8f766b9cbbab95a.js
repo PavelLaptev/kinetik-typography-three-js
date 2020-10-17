17efee1708d889fc6aa3c0f4b1b8a4a1bca1654d
LaptevPavel <pal@alteos.com>
2020-10-17 21:32:35 +0200

    added DOCS folder

---
+++ docs/precache-manifest.1d5a0285dc8b882bc8f766b9cbbab95a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f210a072a10f0506c117a4da94b84920",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2326af701fb6b6948cfc",
+    "url": "./static/css/main.addbdcb4.chunk.css"
+  },
+  {
+    "revision": "31111615a66352615224",
+    "url": "./static/js/2.3f1ee66a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3f1ee66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2326af701fb6b6948cfc",
+    "url": "./static/js/main.f3ec891c.chunk.js"
+  },
+  {
+    "revision": "2222e6bd584a71090272",
+    "url": "./static/js/runtime-main.a998ffbe.js"
+  }
+]);
